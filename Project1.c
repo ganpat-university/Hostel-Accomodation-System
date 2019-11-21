45a1853fe601c00976b5a10d16a79eded7814618
jaivalupadhyay <57015292+jaivalupadhyay@users.noreply.github.com>
2019-11-21 15:21:10 +0530

    Update Project1.c

--- Project1.c
+++ Project1.c
@@ -3,28 +3,30 @@
 
 struct project
 {
-char name[100],history[100],gender[100],college,room[100],status[100],pass[100],type[100],charge[100],age[100],date[100];
-char referral[100];
-int num[100],id[100];
+char name[50],name1[50],history[25],gender[25],college[25],room[25],status[25],pass[25],type[25],charge[25],age[25],date[25];
+char referral[25];
+int num[25],id[25];
 }ob1;
 
-void details_store()
+void details_store()//TO STORE THE STUDENT DETAILS
 {
 printf("Enter Your Room id:");
-scanf("%d",&ob1.id);
-printf("Enter Your Name:");
+scanf("%s",&ob1.id);
+printf("Enter Your First Name:");
 scanf("%s",&ob1.name);
+printf("Enter Your Last Name:");
+scanf("%s",&ob1.name1);
 printf("Enter Your Contact no:");
 scanf("%s",&ob1.num);
-printf("Enter Your Pass Status:");
+printf("Enter Your Pass Status(yes/no):");
 scanf("%s",&ob1.pass);
 printf("Enter Your Age:");
 scanf("%s",&ob1.age);
-printf("Enter Your Medial history:");
+printf("Enter Your Medical History or Conditions:");
 scanf("%s",&ob1.history);
-printf("Enter Your Pass expiry date:");
+printf("Enter Your Pass expiry date(DD/MM/YY):");
 scanf("%s",&ob1.pass);
-printf("Enter Your Gender:");
+printf("Enter Your Gender(Male/Female):");
 scanf("%s",&ob1.gender);
 printf("Enter Your College name:");
 scanf("%s",&ob1.college);
@@ -36,37 +38,58 @@ printf("Enter Your Student Type(engineering/medical):");
 scanf("%s",&ob1.type);
 printf("Enter Your Referral Status(yes/no):");
 scanf("%s",&ob1.referral);
-}
-void display()
-{printf("Room_id");
-printf("\tContact Number");
-printf("\tStudent Name");
-printf("\tPass Status");
+printf("----------------------------------------------------------------------------------------------\n");}
+void display()//TO DISPLAY THE DETAILS//
+{
+printf("Room_id");
+printf("\t\tContact Number");
+printf(" \tStudent Name(First Name)");
+printf("\n%s",ob1.id);
+printf("\t\t%s",ob1.num);
+printf(" \t\t%s",ob1.name);
+printf("\nPass Status");
 printf("\tMedical History");
 printf("\tCollege Name");
-printf("\n");
-printf("%d",ob1.id);
-printf("\t%s",ob1.num);
-printf("\t%s",ob1.name);
-printf("\t%s",ob1.pass);
-printf("\t%s",ob1.history);
-printf("\t%s",ob1.college);
-printf("\n");}
+
+printf("\n%s",ob1.pass);
+printf(" \t\t%s",ob1.history);
+printf("\t\t%s",ob1.college);
+
+}
+
+void output(int *p,int *q,int *r)//TO CALCULATE AND DISPLAY THE ESTIMATE//
+{
+printf("\nAge ");
+printf("\t\tService Charge ");
+printf("\tGender ");
+printf("\t\tDiscount Offered");
+printf("\tTotal Bill");
+printf("\n%s",ob1.age);
+printf("\t\t%d",r);
+printf("\t\t%s",ob1.gender);
+printf("\t\t%d",q);
+printf("\t\t\t%d",p);
+printf("\n----------------------------------------------------------------------------------------------\n");
+}
+//START OF MAIN//
 int main()
 {static int charge = 500;char o[]="yes";char room_type[]="deluxe";int amt=10000;char a[]="yes",Student_Type[]="medical";
- char gender[]="female";char stu[]="new";
+ char gender[]="Female";char stu[]="new";
+int amt_f=0;//Discount
+
 details_store();
 
 display();
 
 if(strcmp(ob1.status,stu)==0)
 {amt=amt-500;
-
+amt_f+=500;
 }
 
 if(strcmp(ob1.type,Student_Type)==0)
 {
 amt=amt-(amt*10/100);
+amt_f+=(amt/10);
 }
 
 
@@ -74,6 +97,7 @@ amt=amt-(amt*10/100);
 if(strcmp(ob1.gender,gender)==0)
 {
 amt=amt-(amt*10/100);
+amt_f+=(amt/10);
 }
 
 
@@ -81,29 +105,21 @@ amt=amt-(amt*10/100);
 if(strcmp(ob1.referral,o)==0)
 {
 amt=amt-(amt*5/100);
+amt_f+=(amt/20);
 }
 
 
 
 if(strcmp(ob1.pass,a)==0)
-{amt=amt-500;
+{amt=amt-500;charge=0;
+amt_f+=500;
 }
 
 if(strcmp(ob1.room,room_type)==0)
 {
 amt=amt+400;
 }
-printf("\nTotal Bill:%d",amt);
-printf("Age:");
-printf("Service Charge:");
-printf("Gender:");
-printf("Discount Offered:");
-printf("Total Bill to be payed:");
-
-printf("%s",ob1.age);
-printf("%s",ob1.charge);
-printf("%s",ob1.gender);
-
+output(amt,amt_f,charge);
+return 0;
 } 
-
-
+//END OF MAIN//
